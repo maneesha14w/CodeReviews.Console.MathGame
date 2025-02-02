c850c42865ba35d4765d3e74c592dda72a6e7c89
Maneesha Walisundera <maneesha14w@gmail.com>
2025-02-02 22:17:59 +0530

    chore: receive input and validation.

--- MathGameCode/MathGame/Program.cs
+++ MathGameCode/MathGame/Program.cs
@@ -1,3 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿string[] validOperators = ["+", "-", "*", "/"];
+bool quit = false;
+while (true && !quit)
+{
+    Console.WriteLine("Please pick an operator you want to play:");
+    Console.WriteLine("+ :: Addition \n- :: Subtraction \n* :: Multiplication \n/ :: Division \nq :: quit");
 
-Console.WriteLine("Hello, World!");
+    var userOperator = Console.ReadLine();
+    var isNotValidOperator = !Array.Exists(validOperators, op => op == userOperator);
+
+    
+    if (!string.IsNullOrWhiteSpace(userOperator))
+    {
+        if (userOperator.Equals("q", StringComparison.OrdinalIgnoreCase))
+        {
+            quit = true;
+            Console.Clear();
+            Console.WriteLine("Thank you for playing :)");
+        }
+        else if (isNotValidOperator)
+        {
+            Console.Clear();
+            Console.WriteLine("Please enter a valid operator");
+        }
+        else
+        {
+            var selectedOperator = userOperator switch
+            {
+                "+" => Operators.Addition,
+                "-" => Operators.Subtraction,
+                "*" => Operators.Multiplication,
+                "/" => Operators.Division,
+                _ => throw new InvalidOperationException("Invalid operator")
+            };
+            DoOperation(selectedOperator);
+        }
+    }
+    else
+    {
+        Console.Clear();
+        Console.WriteLine("Input cannot be empty!");
+
+    }
+    
+}
+
+void DoOperation(Operators op)
+{
+    Console.WriteLine(op);
+}
+
+internal enum Operators
+{
+    Addition,
+    Subtraction,
+    Multiplication,
+    Division
+}
